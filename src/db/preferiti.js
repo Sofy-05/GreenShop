7406f2ce34a481e2526db4c1b21dd163dac7d2bb
Anna Luvisotto <anna.luvisotto@studenti.unitn.it>
2026-02-09 19:05:27 +0100

    revisione preferiti

--- src/db/preferiti.js
+++ src/db/preferiti.js
@@ -13,29 +13,32 @@ router.get('/:user_id', tokenChecker, async (req, res) => { //testata, funziona
         if (idDaUrl !== idDalToken) { //se un utente cerca di visualizzare i preferiti di un altro utente
             return res.status(403).json({
                 success: false,
-                titolo: "Unauthorized",
+                titolo: "Forbidden",
                 dettagli: "Non puoi visualizzare i preferiti di un altro utente"
             });
         }
 
-        const userTrovato = await User.findById(idDaUrl).select('preferitiNegozi').populate('preferitiNegozi'); // .find() usa ciò che gli viene passato per consultare il database
+        const utenteConPreferiti = await User.findById(idDaUrl)
+            .select('preferitiNegozi')
+            .populate('preferitiNegozi', 'nome orari sostenibilitàVerificata coordinate');
 
-        if (!userTrovato) {
+        if (!utenteConPreferiti) {
             return res.status(404).json({
                 success: false,
                 titolo: "Not Found",
-                dettagli: "L'id specificato non corrisponde ad alcun utente"
+                dettagli: "Utente non trovato"
             });
         }
 
-        const listaPulita = userTrovato.preferitiNegozi.filter(negozio => negozio !== null);
-
-        res.status(200).json(userTrovato.preferitiNegozi); //restituisco un messaggio di conferma e la lista degli user trovati (deve essere uno user perché cercato per id)
+        res.status(200).json(utenteConPreferiti.preferitiNegozi);
     }
     catch (err)
     {
-        console.error("Errore nella ricerca dei preferiti: ", err);
-        res.status(500).json({success: false, titolo: "Internal Server Error", dettagli: "Il server fallisce nello stabilire una connessione con il database"})
+        console.error("Errore nella visualizzazione dei preferiti: ", err);
+        res.status(500).json({
+            success: false, 
+            titolo: "Internal Server Error", 
+            dettagli: "Il server fallisce nello stabilire una connessione con il database"})
     }
 });
 
@@ -48,7 +51,7 @@ router.post('/:user_id', tokenChecker, async (req, res) => { //testata, funziona
         if (idDaUrl !== idDalToken) { //se un utente cerca di modificare i preferiti di un altro utente
             return res.status(403).json({
                 success: false,
-                titolo: "Unauthorized",
+                titolo: "Forbidden",
                 dettagli: "Non puoi modificare i preferiti di un altro utente"
             });
         }
@@ -65,7 +68,7 @@ router.post('/:user_id', tokenChecker, async (req, res) => { //testata, funziona
             return res.status(404).json({
                 success: false,
                 titolo: "Not Found",
-                dettagli: "L'id utente non esiste nel database"
+                dettagli: "Utente non trovato"
             });
         }
 
@@ -76,7 +79,10 @@ router.post('/:user_id', tokenChecker, async (req, res) => { //testata, funziona
     catch(err)
     { //gestione errori definiti nelle API
         console.error("Errore nella aggiunta del negozio: ", err);
-        res.status(500).json({success: false, titolo: "Internal Server Error", dettagli: "Il server fallisce nello stabilire una connessione con il database"})
+        res.status(500).json({
+            success: false, 
+            titolo: "Internal Server Error", 
+            dettagli: "Il server fallisce nello stabilire una connessione con il database"})
     } 
 });
 
@@ -88,7 +94,7 @@ router.delete('/:user_id', tokenChecker, async (req, res) => {  //testata, funzi
         if (idDaUrl !== idDalToken) { //se un utente cerca di modificare i preferiti di un altro utente
             return res.status(403).json({
                 success: false,
-                titolo: "Unauthorized",
+                titolo: "Forbidden",
                 dettagli: "Non puoi modificare i preferiti di un altro utente"
             });
         }
