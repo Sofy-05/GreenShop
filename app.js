1a0503a52fdade2c7143b30f0c775b08b33ed72c
Anna Luvisotto <anna.luvisotto@studenti.unitn.it>
2026-02-10 14:47:00 +0100

    modifica per dimensioni foto

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const corsOptions = {
     //su Render leggerà l'URL del frontend (es. https://greenshop-front.onrender.com)
